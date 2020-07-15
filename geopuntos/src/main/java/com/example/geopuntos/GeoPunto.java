340d643ca6405f56e999e0606ecc5cfb9c0b42d1
Camilo Ceron <camiloceron1990@gmail.com>
2020-07-15 17:19:42 -0500

    habilitacion de javadoc y realizaicon de comnetarios en el codigo

--- geopuntos/src/main/java/com/example/geopuntos/GeoPunto.java
+++ geopuntos/src/main/java/com/example/geopuntos/GeoPunto.java
@@ -1,20 +1,28 @@
 package com.example.geopuntos;
-
+/** Clase que reorensenta la longitud y latitud de la tierra */
 public class GeoPunto {
 
     //declaracion de los atributos
+    /** Se define los atributos longitud y latitud de tipo doble */
     private double longitud, latitud;
 
     //declaracion de el contructor
+    /** Le asigna los parametos del constructor a los atributos declarados de la clase GeoPunto */
     public GeoPunto (double longitud, double latitud){
         this.longitud = longitud;
         this.latitud = latitud;
     }
 
     //declaracon del metodo toString
+    /** devuelve una cadena de parametros de tipo String concatenando Latitud y longitud */
+
     public String toString(){
     return latitud + "," + longitud;
     }
+
+    //Declaracion del metoo distancia que
+    /** Calcula la ditancia aproximada de dos coordenadas usando el algoritmo Hersain */
+
 
     public double distancia(GeoPunto punto){
         final double RADIO_TIERRA = 6371000; // en metros
